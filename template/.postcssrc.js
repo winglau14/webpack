fd6ba46b9889ec7afcecd3d475eeba685ae1008c
winglau14 <397069329@qq.com>
2018-01-24 20:24:48 +0800

    init lotus template

--- template/.postcssrc.js
+++ template/.postcssrc.js
@@ -1,10 +1,10 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
+    "plugins": {
+        // to edit target browsers: use "browserlist" field in package.json
+        "autoprefixer": {
+            browsers: ['iOS >= 7', 'Android >= 4.1']
+        }
+    }
 }
--- template/src/main.js
+++ template/src/main.js
@@ -10,6 +10,45 @@ import router from './router'
 {{#vuex}}
 import store from './vuex/store'
 {{/vuex}}
+//rem布局库
+import '../static/lib/newflexible'
+import es6Promise from 'es6-promise'
+//ajax请求
+import lotusAjax from './lotusPlugins/lotusApiRequest'
+//apiUrl地址
+import lotusApiUrl from './lotusPlugins/lotusApiUrl'
+//动态修改微信title
+import lotusChangeTitle from './lotusPlugins/lotusChangeTitle'
+//全站工具类包括了正则验证、页面title、json转字符串、字符串转json
+import lotusUtils from './lotusPlugins/lotusUtils'
+//获取微信签名配置
+import {getWxSignConfig} from './Utils/getWxConfig'
+/*import VueLazyload from 'vue-lazyload'*/
+//loading 组件
+import lotusLoading from './lotusPlugins/lotusLoading'
+//toast 组件
+import lotusToast from './lotusPlugins/lotusToast'
+//lotusNoDataNav 组件
+import lotusNoDataNav from './lotusPlugins/lotusNoDataNav'
+//查看大图组件
+import lotusPreViewer from './lotusPlugins/lotusPreViewer'
+//日期控件
+import lotusCalendar from 'lotus-calendar'
+import 'lotus-calendar/dist/lotus-calendar.min.css'
+import lotusQuery from 'lotus-url-query-string'
+
+es6Promise.polyfill()
+Vue.use(VueRouter)
+Vue.use(lotusAjax)
+Vue.use(lotusApiUrl)
+Vue.use(lotusChangeTitle)
+Vue.use(lotusUtils)
+Vue.use(lotusLoading)
+Vue.use(lotusToast)
+Vue.use(lotusNoDataNav)
+Vue.use(lotusPreViewer)
+Vue.use(lotusCalendar)
+
 
 Vue.config.productionTip = false
 
