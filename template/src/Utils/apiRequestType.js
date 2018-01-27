c4977028f98731783e00ec5b90e3752d7cf64bee
winglau14 <397069329@qq.com>
2018-01-27 17:28:01 +0800

    apiRequestType.js请求优化

--- template/src/Utils/apiRequestType.js
+++ template/src/Utils/apiRequestType.js
@@ -2,6 +2,7 @@ import qs from 'qs'
 import ajaxInterceptors from '../Utils/ajaxInterceptors'
 import AjaxPlugin from 'axios'
 import Vue from 'vue'
+import lotusApiUrl from '../lotusPlugins/lotusApiUrl'
 
 //检测请求状态
 function checkStatus(response) {
@@ -35,49 +36,100 @@ function checkCode(res) {
     if (res.data.code && res.data.code !== 200) {
         Vue.$lotus.toast.show({
             text: res.data.message,
-            show:true,
-            type:'text'
+            show: true,
+            type: 'text'
         });
     } else {
         if (res.data.errorMessage) {
             Vue.$lotus.toast.show({
                 text: res.data.errorMessage,
-                show:true,
-                type:'text'
+                show: true,
+                type: 'text'
             });
         }
     }
     return res
 }
-//get or post请求封装
-export default {
-    get(url, params, contentType){
-        let accessToken = sessionStorage.accessToken;
-        ajaxInterceptors();
-        return AjaxPlugin({
-            method: 'get',
-            url: url,
-            params, // get 请求时带的参数
-            timeout: 30000,
-            headers: {
-                'Content-Type': contentType||'application/x-www-form-urlencoded',
-                //'Authorization': `Bearer ${btoa(accessToken)}`
-            }
-        }).then(checkStatus).then(checkCode);
-    },
-    post(url, data, contentType){
-        let accessToken = sessionStorage.accessToken;
-        ajaxInterceptors();
-        const postData = contentType ? data : qs.stringify(data);
+
+//获取token
+function getToken() {
+    //第一次请求token
+    if(!sessionStorage.accessToken){
         return AjaxPlugin({
             method: 'post', // 请求协议
-            url: url, // 请求的地址
-            data: postData, // post 请求的数据
+            url: lotusApiUrl.$lotusApiUrl.Auth, // 请求的地址
+            data: qs.stringify({
+                "client_id": "NXPlfsmzc7jwq4xanhu",
+                "client_secret": "u82e9lys6icpjw04gtn1rahfzqb3mdkx",
+                "grant_type": "client_credentials"
+            }), // post 请求的数据
             timeout: 30000, // 超时时间, 单位毫秒
             headers: {
-                'Content-Type': contentType||'application/x-www-form-urlencoded; charset=UTF-8',
-                //'Authorization': `Bearer ${btoa(accessToken)}`
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
             }
-        }).then(checkStatus).then(checkCode);
+        }).then((response) => {
+            const result = response.data;
+            if (result) {
+                if (result.access_token && result.refresh_token) {
+                    sessionStorage.accessToken = response.data.access_token;
+                    sessionStorage.refreshToken = response.data.refresh_token;
+                    sessionStorage.resultToken = JSON.stringify(result);
+                    return result;
+                }
+            }
+        });
+    }else{
+        //缓存token
+        return new Promise((resolve,reject)=>{
+            //获取缓存token并return result
+            if(sessionStorage.resultToken){
+                const result = JSON.parse(sessionStorage.resultToken);
+                resolve(result);
+            }
+        })
+    }
+}
+
+//get方法
+function getFetch(url, params, contentType, accessToken) {
+    return AjaxPlugin({
+        method: 'get',
+        url: url,
+        params, // get 请求时带的参数
+        timeout: 30000,
+        headers: {
+            'Content-Type': contentType || 'application/x-www-form-urlencoded',
+            'Authorization': `Bearer ${(accessToken)}`
+        }
+    }).then(checkStatus).then(checkCode);
+}
+
+//post方法
+function postFetch(url, data, contentType, accessToken) {
+    const postData = contentType ? data : qs.stringify(data);
+    return AjaxPlugin({
+        method: 'post', // 请求协议
+        url: url, // 请求的地址
+        data: postData, // post 请求的数据
+        timeout: 30000, // 超时时间, 单位毫秒
+        headers: {
+            'Content-Type': contentType || 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Authorization': `Bearer ${(accessToken)}`
+        }
+    }).then(checkStatus).then(checkCode);
+}
+//get or post请求封装
+export default {
+    async get(url, params, contentType){
+        //获取token
+        const result = await getToken();
+        ajaxInterceptors();
+        return await getFetch(url, params, contentType, result.access_token);
+    },
+    async post(url, data, contentType){
+        //获取token
+        const result = await getToken();
+        ajaxInterceptors();
+        return await postFetch(url, data, contentType, result.access_token);
     }
 }
